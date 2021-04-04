8c06b2a44d23367aa2e0562c1eed7d3c2822bdbd
Hanibal Girmay <hanibalgirmay@gmail.com>
2021-04-04 16:14:44 +0300

    Laravel API using sanctum authentication

---
+++ database/migrations/2021_04_03_182958_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_Visible')->default(true);
+            $table->boolean('is_Sold')->default(false);
+            $table->string('description')->nullable();
+            $table->string('tags');
+            $table->decimal('price', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
